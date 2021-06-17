367e878580be9f8c88758c55d2c9cdf1a4acc18b
arnabde319 <84794810+arnabde319@users.noreply.github.com>
2021-06-17 17:17:27 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -14,7 +14,7 @@ int max(int arr[], int len) {
 int min(int arr[], int len) {
   int min=arr[0];
   for(int i = 1; i<len ; i++){
-    if(arr[i]<max){
+    if(arr[i]<min){
       min=arr[i];
     }
   }
